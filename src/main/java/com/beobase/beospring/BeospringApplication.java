0b750750b48c1c03010b1448b399b2e2866c2d16
Bellamy Phan <bellamyphan@icloud.com>
2026-08-06 17:32:16 -0500

    BEO-166: Initial commit BeoSpring

---
+++ src/main/java/com/beobase/beospring/BeospringApplication.java
@@ -0,0 +1,13 @@
+package com.beobase.beospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beobase/beospring/BeospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beobase.beospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
