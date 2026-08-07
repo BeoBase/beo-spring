9936912186b6a43f2e7e9f9dceb59c4fd687140f
Bellamy Phan <55559722+bellamyphan@users.noreply.github.com>
2026-08-07 12:22:45 -0500

    Beo 180 squash commit only (#5)
    * BEO-180: testing comment 1
    
    * BEO-180: testing comment 2

--- src/main/java/com/beobase/beospring/BeospringApplication.java
+++ src/main/java/com/beobase/beospring/BeospringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeospringApplication {
 
 	static void main(String[] args) {
+		// Testing comment 2
 		SpringApplication.run(BeospringApplication.class, args);
 	}
 
