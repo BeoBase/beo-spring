0c936223a45930e5afafffc6f9402ae716da8ae1
Bellamy Phan <bellamyphan@icloud.com>
2026-08-07 12:18:51 -0500

    BEO-180: testing comment 1

--- src/main/java/com/beobase/beospring/BeospringApplication.java
+++ src/main/java/com/beobase/beospring/BeospringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeospringApplication {
 
 	static void main(String[] args) {
+		// Testing comment 1
 		SpringApplication.run(BeospringApplication.class, args);
 	}
 
