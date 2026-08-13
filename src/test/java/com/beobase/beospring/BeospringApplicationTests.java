8e825cedaf8c3aef862ab4104b35109cba011b94
Bellamy Phan <bellamyphan@icloud.com>
2026-08-13 14:06:52 -0500

    BEO-299: checking testing coverage when try to delete some files

--- src/test/java/com/beobase/beospring/BeospringApplicationTests.java
+++ src/test/java/com/beobase/beospring/BeospringApplicationTests.java
@@ -1,18 +1,18 @@
-package com.beobase.beospring;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BeospringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void mainStartsApplication() {
-		BeospringApplication.main(new String[]{});
-	}
-
-}
+//package com.beobase.beospring;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BeospringApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	@Test
+//	void mainStartsApplication() {
+//		BeospringApplication.main(new String[]{});
+//	}
+//
+//}
