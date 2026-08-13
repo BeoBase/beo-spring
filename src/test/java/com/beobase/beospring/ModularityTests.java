7976df65f0e00a8aa909bb55878c34ac48c10cc3
Bellamy Phan <bellamyphan@icloud.com>
2026-08-13 13:16:43 -0500

    BEO-293: add ModularityTests to enforce Spring Modulith

---
+++ src/test/java/com/beobase/beospring/ModularityTests.java
@@ -0,0 +1,12 @@
+package com.beobase.beospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+
+public class ModularityTests {
+
+    @Test
+    void verifiesModularStructure() {
+        ApplicationModules.of(BeospringApplication.class).verify();
+    }
+}
