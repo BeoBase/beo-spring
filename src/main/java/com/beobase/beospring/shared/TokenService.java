7b248fe0c30063c410fba0c13d7ea9d714403876
Bellamy Phan <bellamyphan@icloud.com>
2026-08-28 23:34:21 -0500

    BEO-315: update token service to generate token for valid user, include unit tests

--- src/main/java/com/beobase/beospring/shared/TokenService.java
+++ src/main/java/com/beobase/beospring/shared/TokenService.java
@@ -1,8 +1,13 @@
 package com.beobase.beospring.shared;
 
+import java.util.function.Predicate;
+
 public interface TokenService {
 
-    String generate(String userId);
+    String generate(String userId, String email, String role);
     String extractUserId(String token);
+    String extractEmail(String token);
+    String extractRole(String token);
     boolean isValid(String token);
+    String validateTokenAndGetUserId(String token, Predicate<String> userExists);
 }
